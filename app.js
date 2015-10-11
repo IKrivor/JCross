af821b959276b178ff5f32ae4ee1d54675cf0b79
Ирина Криворотова <kin2805@yandex.ru>
2015-10-11 07:27:46 +0600

    Создать меню, страницы с кроссвордами и с правилами, реализовать переключение между ними

---
+++ app.js
@@ -0,0 +1,19 @@
+var App = angular.module('PaperGenerator', ['ngRoute', 'ngMaterial']);
+
+App.config(function ($routeProvider, $mdThemingProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'templates/generator.html',
+            controller: 'GeneratorCtrl'
+        })
+        .otherwise({
+            redirectTo: '/'
+        });
+
+     $mdThemingProvider.theme('default').accentPalette('green', {
+     	default: '400'
+    });
+});
+
+angular.module('whiteFrameBasicUsage', ['ngMaterial']);
+
---
+++ controllers/GeneratorCtrl.js
@@ -0,0 +1,10 @@
+App.controller('GeneratorCtrl', ['$scope', '$mdSidenav', function($scope){
+	$scope.data = {
+		navigation: 'cross'
+	};
+
+	var actionLeft1=true;
+	var actionRight1=false;
+
+}]);
+
