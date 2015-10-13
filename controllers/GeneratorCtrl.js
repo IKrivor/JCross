2ad7a5f73dcba0cbd986b29289318569901f29b1
Ирина Криворотова <kin2805@yandex.ru>
2015-10-13 22:46:00 +0600

    Добавить лого и превью в интерфейс

--- controllers/GeneratorCtrl.js
+++ controllers/GeneratorCtrl.js
@@ -1,10 +1,9 @@
 App.controller('GeneratorCtrl', ['$scope', '$mdSidenav', function($scope){
-	$scope.data = {
-		navigation: 'cross'
-	};
 
 	var actionLeft1=true;
 	var actionRight1=false;
+
+
 
 }]);
 
