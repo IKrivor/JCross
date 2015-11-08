7f3a39cab8c6340e774b19b6d88f1b83eb103aa8
Ирина Криворотова <kin2805@yandex.ru>
2015-11-08 19:13:32 +0600

    Перенести функции отрисовки сетки в файл js

--- controllers/GeneratorCtrl.js
+++ controllers/GeneratorCtrl.js
@@ -67,5 +67,110 @@ App.controller('GeneratorCtrl', ['$scope', '$mdSidenav', function($scope){
 	}
 
 
+
+
 }]);
 
+function drawleft(){
+	var lh = document.getElementById("lh");
+	var conte = lh.getContext("2d");
+	var ulleft = [
+		[0,0,0,0,4],
+		[0,0,0,0,6],
+		[0,1,1,2,2],
+		[1,1,2,2,2],
+		[1,1,2,2,2],
+		[0,3,2,1,2],
+		[0,0,3,2,4],
+		[0,0,3,3,3],
+		[0,0,0,0,11],
+		[0,0,0,0,15]
+	];
+	var ullx = 10, ully = 5;
+	for (var x = 0.5; x < 101; x += 20) {//вертикальные линии
+		conte.moveTo(x, 0);
+		conte.lineTo(x, 201);
+	}
+	for (var y = 0.5; y < 201; y += 20) {//горизонтальные линии
+		conte.moveTo(0, y);
+		conte.lineTo(101, y);
+	}
+	conte.stroke();
+	conte.font = "bold 12px sans-serif";
+	conte.textBaseline = "top";
+	var a = 5, b = 5;
+	for (var i = 0; i < ully; i++) {
+		a = 5 + (20*i);
+		for (var j = 0; j < ullx; j++) {
+			b = 5 + (20*j);
+			if (ulleft[j][i] != 0){
+				conte.fillText(ulleft[j][i], a, b);
+			}
+		}
+	}
+}
+
+function drawtop(){
+	var ultop = [
+		[0,0,0,0,0,0,0,0,2,2,0,0,0,0,0],
+		[3,3,0,0,0,0,0,3,4,2,3,0,3,0,0],
+		[1,1,8,5,2,7,9,3,2,4,4,9,2,1,1]
+	];
+	var ultx = 3, ulty = 15;
+	var th = document.getElementById("th");
+	var cont = th.getContext("2d");
+	for (var x = 0.5; x < 301; x += 20) {//вертикальные линии
+		cont.moveTo(x, 0);
+		cont.lineTo(x, 61);
+	}
+	for (var y = 0.5; y < 61; y += 20) {//горизонтальные линии
+		cont.moveTo(0, y);
+		cont.lineTo(301, y);
+	}
+	cont.stroke();
+	cont.font = "bold 12px sans-serif";
+	cont.textBaseline = "top";
+	var a = 5, b = 5;
+	for (var i = 0; i < ulty; i++) {
+		a = 5 + (20*i);
+		for (var j = 0; j < ultx; j++) {
+			b = 5 + (20*j);
+			if (ultop[j][i] != 0){
+				cont.fillText(ultop[j][i], a, b);
+			}
+		}
+	}
+}
+function drawbody(){
+	var ulitka = [
+		[0,0,0,0,0,0,0,1,1,1,1,0,0,0,0],
+		[0,0,0,0,0,0,1,1,1,1,1,1,0,0,0],
+		[1,0,1,0,0,0,1,1,0,0,1,1,0,0,0],
+		[1,0,1,0,0,1,1,0,1,1,0,1,1,0,0],
+		[1,0,1,0,0,1,1,0,1,1,0,1,1,0,0],
+		[0,1,1,1,0,1,1,0,1,0,0,1,1,0,0],
+		[0,1,1,1,0,1,1,0,1,1,1,1,0,0,0],
+		[0,1,1,1,0,1,1,1,0,1,1,1,0,0,0],
+		[0,0,1,1,1,1,1,1,1,1,1,1,1,0,0],
+		[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+	];
+	var ul = document.getElementById("ul");
+	var context = ul.getContext("2d");
+	for (var x = 0.5; x < 301; x += 20) {//вертикальные линии
+		context.moveTo(x, 0);
+		context.lineTo(x, 200);
+	}
+	for (var y = 0.5; y < 201; y += 20) {//горизонтальные линии
+		context.moveTo(0, y);
+		context.lineTo(301, y);
+	}
+	context.stroke();
+	function halmaOnClick(e) {
+		var x1;
+		var y1;
+		x1 = (e.pageX - ul.offsetLeft) / 20 | 0;
+		y1 = (e.pageY - ul.offsetTop) / 20 | 0;
+		context.fillRect(x1*20, y1*20, 20, 20);
+	}
+	ul.addEventListener("click", halmaOnClick, false);
+}
